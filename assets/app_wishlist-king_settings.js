7ade6c6b12bc55ded1ef4444cfad4fd660bd2688
arpitsingh7339 <arpitsinghyadav9876543210@gmail.com>
2025-09-26 20:27:10 +0530

    before

---
+++ assets/app_wishlist-king_settings.js
@@ -0,0 +1,1 @@
+export default {"facebookPixel":true,"googleAnalytics":true,"klaviyo":true,"loginRequired":false,"moveToCart":false,"sharedWishlistPageHandle":"shared-wishlist","wishlistPageHandle":"wishlist"}
